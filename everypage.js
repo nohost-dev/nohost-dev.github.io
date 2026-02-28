f762bade353443c7fd28db300d032da69ccbb783
Andrew Buchan <andyhasit@gmail.com>
2026-02-28 13:18:55 +0000

    Added initial files

---
+++ everypage.js
@@ -0,0 +1,130 @@
+const getEl = (id) => {
+  const element = document.getElementById(id);
+  if (!element) {
+    throw new Error("Could not find element with id " + id);
+  }
+  return element;
+};
+
+const fetchJson = (path) =>
+  fetch(path, {
+    method: "get",
+  }).then((response) => response.json());
+
+function Menu() {
+  this.wrapperDiv = getEl("menu-wrapper");
+  this.overlayDiv = getEl("menu-overlay");
+  this.mainMenuTab = getEl("main-menu-tab");
+  this.appMenuTab = getEl("app-menu-tab");
+  this.appMenuEntries = getEl("app-menu-entries");
+  this.mainMenuApps = getEl("main-menu-apps");
+  this.showAppTab();
+}
+
+Menu.prototype = {
+  show: function () {
+    this.showAppTab();
+    this.wrapperDiv.classList.add("visible");
+    this.overlayDiv.classList.add("visible");
+  },
+  hide: function () {
+    this.wrapperDiv.classList.remove("visible");
+    this.overlayDiv.classList.remove("visible");
+  },
+  buildMenuEntry: function (entry) {
+    const element = document.createElement("a");
+    element.href = entry[0];
+    element.innerHTML = entry[1];
+    return element;
+  },
+  noop: function (event) {
+    event.stopPropagation();
+  },
+  showAppTab: function () {
+    this.mainMenuTab.hidden = true;
+    this.appMenuTab.hidden = false;
+  },
+  showMainTab: function () {
+    this.mainMenuTab.hidden = false;
+    this.appMenuTab.hidden = true;
+  },
+  addApps: function (apps) {
+    apps.forEach((entry) => {
+      this.mainMenuApps.appendChild(this.buildMenuEntry(entry));
+    });
+  },
+};
+
+window.com = {
+  init: function () {
+    this.appDiv = getEl("app");
+    this.menu = new Menu();
+    this.loadSettings();
+    this.startLoadingTimeOut();
+  },
+  loadSettings: function () {
+    fetchJson("/your/settings.json").then((data) => {
+      this.settings = data;
+      this.menu.addApps(
+        data.apps.map((app) => [`/your/apps/${app.path}`, app.menu])
+      );
+    });
+  },
+  startLoadingTimeOut: function () {
+    com._timeout = setTimeout(com.showOfflineMessage, 25000);
+  },
+  clearLoadingTimeOut: function () {
+    clearTimeout(com._timeout);
+  },
+  deadClick: function (event) {
+    event.stopPropagation();
+  },
+  showOfflineMessage: function () {
+    com.appDiv.innerHTML = "<h3>Page timed out</h3>";
+  },
+  buildMenuEntry: function (entry) {
+    return '<a href="' + entry[0] + '">' + entry[1] + "</a>";
+  },
+  goto: function (route) {
+    window.location.href = route;
+  },
+};
+
+window.onload = function () {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => console.log("Service worker registered."));
+  }
+  com.init();
+};
+
+/*
+Fake Dropbox which goes to data dir via webpack.
+*/
+
+function Dropbox() {
+  this.base = "/api/dbx/";
+}
+
+Dropbox.prototype = {
+  fetch(path, opts) {
+    return fetch(this.base + path.replace(/^\/+/, ""), opts);
+  },
+  fetchFile: function (path) {
+    this.fetch(path).then((response) => response.body);
+  },
+  saveFile: function (path, data) {
+    console.log("Saving", path);
+  },
+  fetchJson: function (path) {
+    return this.fetch(path, { method: "get" }).then((response) =>
+      response.json().then((rjson) => JSON.parse(rjson.data))
+    );
+  },
+  saveJson: function (path, data) {
+    return this.fetch(path, { method: "post", body: JSON.stringify(data) });
+  },
+};
+
+window.dbx = new Dropbox();
---
+++ sw.js
@@ -0,0 +1,216 @@
+/**
+ * Must be served from root.
+ */
+
+const VERSION = "1";
+const staticCacheName = `${VERSION}`;
+const userAppPattern = /^your\/apps\/.+/;
+const placeholders = {
+  head: "__REPLACE_HEAD__",
+  body: "__REPLACE_BODY__",
+};
+
+const noHostRoutes = {
+  settings: {
+    head: [{ type: "link", href: "/settings.css" }],
+    body: [{ type: "script", src: "/settings.js" }],
+  },
+};
+
+console.log("[SW] Started");
+/**
+ * The install event triggers once for every new version of the service worker,
+ * which should have an updated app_version?
+ *
+ * The new service worker will not be controlling the page yet, so we cannot
+ * discrupt the existing one and its caches.
+ *
+ * At this point we cache the minimum files required to run.
+ */
+self.addEventListener("install", (event) => {
+  console.log("[SW] Installing SW version:", VERSION);
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      // getFile('/hello.txt')
+      console.log("[SW] Caching app shell");
+      // return cache.addAll([
+      //   new Request("{{ turbopage_template }}", { cache: "no-cache" }),
+      //   "/manifest.json"
+      // ]);
+    })
+  );
+});
+
+/**
+ * This intercepts network requests, including navigation.
+ * You really want to preserve console log in dev tools when debugging.
+ */
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  console.log("[SW] nav to: ", request.mode, request.url);
+  if (request.mode == "navigate") {
+    const controlledRoute = getControlledRoute(request.url);
+    if (controlledRoute) {
+      event.respondWith(getControlledRouteResponses(controlledRoute));
+    }
+  }
+  // Handle static assests
+  // if (requestShouldBeCached(request)) {
+  //   event.respondWith(cacheThenNetwork(event));
+  // }
+});
+
+async function getControlledRouteResponses(controlledRoute) {
+  const isUserAppRoute = userAppPattern.test(controlledRoute);
+  const toRemove = isUserAppRoute ? "your/apps/" : "your/";
+  const shortPath = controlledRoute.slice(toRemove.length);
+  const routeMap = isUserAppRoute ? await userRoutes() : noHostRoutes;
+
+  console.log(routeMap, shortPath);
+  const matchingRoute = routeMap.find((entry) => entry.path === shortPath);
+  if (matchingRoute) {
+    return buildAppResponse(matchingRoute);
+  } else {
+    const msg = isUserAppRoute ? "is not an app" : "not found";
+    return new Response(
+      `Service worker failed to find path:\n\n${controlledRoute} ${msg}.`
+    );
+  }
+}
+
+async function getRouteManifest(manifestPath) {
+  const response = await fetch(manifestPath);
+  const data = await response.json();
+  const base = manifestPath.substring(0, manifestPath.lastIndexOf("/"));
+  return { ...data, base };
+}
+
+async function buildAppResponse(route) {
+  const manifest = await getRouteManifest(route.manifest);
+  const { base } = manifest;
+  head = assetsToString(manifest.head, base);
+  body = assetsToString(manifest.body, base);
+  const response = await fetch("/skeleton.html");
+  const bodyStream = response.body
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(streamingReplace(placeholders.head, head))
+    .pipeThrough(streamingReplace(placeholders.body, body))
+    .pipeThrough(new TextEncoderStream());
+  return new Response(bodyStream, response);
+}
+
+const isRelativePath = (path) =>
+  !(path.startsWith("/") || path.startsWith("http"));
+
+function assetsToString(assets, base) {
+  if (typeof assets === "string") return assets;
+  if (Array.isArray(assets)) {
+    let str = "";
+    assets.forEach((asset) => {
+      let path = asset.path;
+      if (isRelativePath(path)) path = base + "/" + path;
+      if (asset.type === "script") {
+        str += `<script src="${path}"></script>`;
+      } else if (asset.type === "link") {
+        str += `<link rel="stylesheet" href="${path}" />`;
+      } else {
+        throw new Error("Unknown asset type: " + asset.type);
+      }
+    });
+    return str;
+  } else {
+    throw new Error("Assets must be an array or string");
+  }
+}
+
+/**
+ * Returns a TransformStream which does a find & replace.
+ */
+const streamingReplace = (find, replace) => {
+  let buffer = "";
+
+  return new TransformStream({
+    transform(chunk, controller) {
+      buffer += chunk;
+      let outChunk = "";
+
+      while (true) {
+        const index = buffer.indexOf(find);
+        if (index === -1) break;
+        outChunk += buffer.slice(0, index) + replace;
+        buffer = buffer.slice(index + find.length);
+      }
+
+      outChunk += buffer.slice(0, -(find.length - 1));
+      buffer = buffer.slice(-(find.length - 1));
+      controller.enqueue(outChunk);
+    },
+    flush(controller) {
+      if (buffer) controller.enqueue(buffer);
+    },
+  });
+};
+
+async function userRoutes() {
+  const settings = await userSettings();
+  return settings["apps"];
+}
+
+async function userSettings() {
+  const response = await fetch("/your/settings.json").catch((err) => {
+    console.error(err);
+  });
+  const settings = await response.json().catch((err) => {
+    console.error(err);
+  });
+  return settings;
+}
+
+/**
+ * Returns request route excluding query params and hash:
+ *
+ * e.g. "0.0.0.0/hello/you?name=bob#home" > "hello/you"
+ */
+function relativePath(url) {
+  const end = url.substr(location.origin.length + 1);
+  return end.split("?")[0].split("#")[0];
+}
+
+/**
+ * Returns the app route e.g. "your/settings" or "your/apps/app1" if
+ * the url is an app route (starts with `your/`, else undefined.
+ */
+function getControlledRoute(url) {
+  if (url.startsWith(location.origin)) {
+    const route = relativePath(url);
+    if (route.startsWith("your/") && !route.endsWith(".json")) {
+      return route;
+    }
+  }
+}
+
+/**
+ * This tries the cache first, failing that it goes to network
+ * and returns that, and also caches that response.
+ */
+const cacheThenNetwork = (event) => {
+  return caches
+    .match(event.request) // checks all caches
+    .then((response) => {
+      if (response) {
+        console.log(`[SW] Served from cache: ${event.request.url}`);
+        return response;
+      } else {
+        return caches.open(staticCacheName).then(function (cache) {
+          console.log(`[SW] Requested from network: ${event.request.url}`);
+          return fetch(event.request).then(function (response) {
+            console.log(`[SW] Added to cache: ${event.request.url}`);
+            if (response.ok) {
+              cache.put(event.request, response.clone());
+            }
+            return response;
+          });
+        });
+      }
+    });
+};
