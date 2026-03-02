5f34c15550bf78d6a4ecef6025b760f68442d157
Andrew Buchan <andyhasit@gmail.com>
2026-03-02 16:36:03 +0000

    add data and duration

--- apps/demo/index.tsx
+++ apps/demo/index.tsx
@@ -12,11 +12,14 @@ const Task: Uses<iTask> = ({ title, done, id }) => (
     <input type="checkbox" bind:checked={done} />
   </div>
 );
+let duration = 0;
 
 const TaskList: Uses<iTask[]> = (tasks) => (
   <div>
     <h3>Tasks with update</h3>
+    <div>Duration: {duration}</div>
     <Task.repeat props={tasks} />
+    <button onClick={addData()}>add</button>
   </div>
 );
 
@@ -28,13 +31,27 @@ const TaskList: Uses<iTask[]> = (tasks) => (
 // }
 // load();
 
+let settings;
+const start = performance.now();
 window.onload = function () {
   const root = mount("app", TaskList, []);
-  dbx.getJson("/tasks.json").then((settings) => {
+  dbx.getJson("/tasks.json").then((res) => {
+    duration = performance.now() - start;
+    settings = res;
     const tasks = watch(settings.tasks, () => {
       dbx.putJson("/tasks.json", settings);
       root.update();
     });
     root.render(tasks);
   });
+};
+
+const addData = () => {
+  settings.other = Array(1000)
+    .fill(0)
+    .map((_, i) => ({
+      task: `task ${i}`,
+      done: i % 2 === 0,
+    }));
+  dbx.putJson("/tasks.json", settings);
 };
